aa7e43f326512d74d28ade7f99c0f19dcb0c6776
André Gustavo <andre.belo@ufpe.br>
2022-08-25 15:14:42 -0300

    Date Select

--- backend/src/main/java/com/plansoftonline/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/plansoftonline/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.plansoftonline.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.plansoftonline.dsmeta.entities.Sale;
@@ -14,8 +18,18 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public Page<Sale> findSales(Pageable page) {
+		return saleRepository.findAll(page);
+	}
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable page) {
+		
+		LocalDate hoje = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()); //criar uma data com dia de hj
+		
+		LocalDate min = minDate.equals("") ? hoje.minusDays(365) : LocalDate.parse(minDate); //Se a data for vazia pega a data atual - 365 dias
+		LocalDate max = maxDate.equals("") ? hoje : LocalDate.parse(maxDate); //Se a data for vazia pega a data atual (hoje)
+		
+		return saleRepository.findSales(min, max, page);
 	}
 
 }
